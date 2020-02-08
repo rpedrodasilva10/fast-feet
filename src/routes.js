9979f3d653712807b7bf5bef2c0ac5335031cc3b
Renan Pedro da Silva <rpedrodasilva10@gmail.com>
2020-02-08 20:27:04 -0300

    feat: add JWT on login, SessionController store

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => {
   res.json('hello world');
 });
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
