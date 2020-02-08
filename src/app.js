3ad07322f6b31212c7359de211eb03c7bd0250bc
Renan Pedro da Silva <rpedrodasilva10@gmail.com>
2020-02-08 16:52:09 -0300

    Add eslint, prettier and editorconfig to fix and  enforce code style

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import express from "express";
-import routes from "./routes";
+import express from 'express';
+import routes from './routes';
 
 class App {
   constructor() {
@@ -17,5 +17,4 @@ class App {
   }
 }
 
-//module.exports = new App().server;
 export default new App().server;
